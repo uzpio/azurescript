bb755577339c869180e9db507fd34768874eacf5
uzpio <63773874+uzpio@users.noreply.github.com>
2020-05-13 15:56:06 +0200

    Update gatewayInstall.ps1

--- gatewayInstall.ps1
+++ gatewayInstall.ps1
@@ -226,9 +226,9 @@ function Register-Gateway([string] $instanceKey)
 
 
 Trace-Log "Log file: $logLoc"
-$uri = "https://dhubgsddevdf.blob.core.windows.net/binaries/IntegrationRuntime_4.7.7383.1.msi"
-$urij = "https://dhubgsddevdf.blob.core.windows.net/binaries/jre-8u251-windows-x64.exe"
-$uric = "https://dhubgsddevdf.blob.core.windows.net/binaries/config.txt"
+$uri = "https://binfordeploy.blob.core.windows.net/binaries/IntegrationRuntime_4.7.7383.1.msi"
+$urij = "https://binfordeploy.blob.core.windows.net/binaries/jre-8u251-windows-x64.exe"
+$uric = "https://binfordeploy.blob.core.windows.net/binaries/config.txt"
 
 Trace-Log "Gateway download fw link: $uri"
 $gwPath= "$PWD\gateway.msi"
